96c086f5890de403df1cf03c1f303a8ab49c8265
Divya Sree Karanam <119149642+DivyasreeKaranam@users.noreply.github.com>
2024-10-22 18:00:05 +0530

    Main.java

--- bunchbysoh/Main.java
+++ bunchbysoh/Main.java
@@ -9,13 +9,35 @@ public class Main {
 
   static CountsBySoH countBatteriesByHealth(int[] presentCapacities) {
     CountsBySoH counts = new CountsBySoH();
+    double rated_capacity=120.0;
+    for(int capacity:presentCapacities)
+    {
+      double soh=(capacity/rated_capacity)*100.0;
+      if (soh>83.0 && soh<=100.0)
+      {
+        counts.healthy++;
+      }
+      else if (soh>=63.0 && soh<=83.0)
+      {
+        counts.exchange++;
+      }
+      else
+      {
+        counts.failed++;
+      }
+
+    }
     return counts;
   }
 
+
   static void testBucketingByHealth() {
     System.out.println("Counting batteries by SoH...\n");
-    int[] presentCapacities = {113, 116, 80, 95, 92, 70};
+    int[] presentCapacities = {113, 116, 80, 95, 92, 70, 56};
     CountsBySoH counts = countBatteriesByHealth(presentCapacities);
+    System.out.println("Healthy: " + counts.healthy);
+    System.out.println("Exchange: " + counts.exchange);
+    System.out.println("Failed: " + counts.failed);
     assert(counts.healthy == 2);
     assert(counts.exchange == 3);
     assert(counts.failed == 1);
